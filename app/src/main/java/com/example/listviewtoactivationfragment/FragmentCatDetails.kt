546c62b57a4b4f2124c48e635a5c2db56858eee5
KashbullinRail <kashbullinrail@gmail.com>
2022-11-27 18:42:44 +0300

    реализован фрагмент FragmentCatDetails.kt

--- app/src/main/java/com/example/listviewtoactivationfragment/FragmentCatDetails.kt
+++ app/src/main/java/com/example/listviewtoactivationfragment/FragmentCatDetails.kt
@@ -5,19 +5,38 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
+import com.example.listviewtoactivationfragment.databinding.FragmentCatDetailsBinding
+import com.example.listviewtoactivationfragment.databinding.FragmentCatsBinding
+import com.example.listviewtoactivationfragment.model.Cat
 
 class FragmentCatDetails : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    private lateinit var binding: FragmentCatDetailsBinding
 
-    }
+    val cat:Cat
+    get() = requireArguments().getSerializable(KEY_CAT) as Cat
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_cat_details, container, false)
+        binding = FragmentCatDetailsBinding.inflate(inflater, container, false)
+
+        binding.tvName.text = cat.name
+        binding.tvDescription.text = cat.discription
+
+        return binding.root
+    }
+
+    companion object {
+        private const val KEY_CAT = "KEY_CAT"
+    }
+
+    fun newInstance(cat:Cat): FragmentCatDetails {
+        val fragment = FragmentCatDetails()
+        fragment.arguments = bundleOf(KEY_CAT to cat)
+        return fragment
     }
 
 }
--- app/src/main/java/com/example/listviewtoactivationfragment/model/Cat.kt
+++ app/src/main/java/com/example/listviewtoactivationfragment/model/Cat.kt
@@ -1,10 +1,12 @@
 package com.example.listviewtoactivationfragment.model
 
+import java.io.Serializable
+
 data class Cat(
     val id:Int,
     val name:String,
     val discription:String
-){
+):Serializable {
     override fun toString(): String {
         return name
     }
